ccf9d8a7b4605baa135e21f7ee46bf3a56e979e5
Domiii <dominik.seifert@gmail.com>
2018-01-18 17:50:26 +0800

    - added third deform method to 03_CreatOMat1, using noise to scale vertex along normal

--- 03_CreatOmat1/Assets/Scripts/NoisyMesh.cs
+++ 03_CreatOmat1/Assets/Scripts/NoisyMesh.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NoisyMesh : MonoBehaviour {
-	public float speed = .5f;
-	public float scale = .3f;
+	public float speed = 2;
+	public float scale = .1f;
+
+	/// <summary>
+	/// The higher, the less detail you will see, unless you have a lot of octaves.
+	/// </summary>
+	public float frequency = 1;
+
+	/// <summary>
+	/// The amount of levels of detail you want to see (each twice as finely scaled as the previous)
+	/// </summary>
+	public int octaves = 8;
 	public int seed = 23455898;
 
 	public bool isMoving = true;
@@ -13,26 +23,82 @@ public class NoisyMesh : MonoBehaviour {
 	float seedX, seedY, seedZ;
 
 	MeshFilter meshFilter;
-	Perlin noise = new Perlin ();
+	Perlin perlin;
 	Vector3[] baseVertices;
+	Vector3[] baseNormals;
 
 	void Awake() {
 		meshFilter = GetComponent<MeshFilter> ();
 
+		var mesh = meshFilter.mesh;
+		baseVertices = mesh.vertices;
+		baseNormals = mesh.normals;
+
 		Random.InitState (seed);
-		seedX = Random.Range (0, 1);
-		seedY = Random.Range (2, 4);
-		seedZ = Random.Range (5, 7);
+		seedX = Random.Range (1.0f, 2) * 14486;
+		seedY = Random.Range (1.0f, 2) * 47940;
+		seedZ = Random.Range (1.0f, 2) * 98593;
+
+		perlin = new Perlin ();
 
 		time = Time.time;
 	}
 
 	void Update() {
-		Deform2 ();
+		Deform3 ();
 
 		if (isMoving) {
 			time += Time.deltaTime;
 		}
+	}
+
+	static float min = -.9f, max= .9f;
+
+	/// <summary>
+	/// Extend mesh along normal
+	/// </summary>
+	void Deform3() {
+		var mesh = meshFilter.mesh;
+
+		var vs = new Vector3[baseVertices.Length];
+		//var vs = mesh.vertices;
+
+		var offsetX = speed * time + seedX;
+		var offsetY = speed * time + seedY;
+		var offsetZ = speed * time + seedZ;
+
+		for (var i = 0; i < vs.Length; ++i) {
+			var u = baseVertices[i];
+			var v = vs [i];
+
+			var inX = u.x;
+			var inY = u.y;
+			var inZ = u.z;
+
+			float delta = 0;
+			var gain = 1.0f;
+			for(var o = 0; o < octaves; o++)
+			{
+				delta += perlin.Noise (offsetX + inX*gain/frequency, offsetY + inY*gain/frequency, offsetZ + inZ*gain/frequency) / gain;
+				gain *= 2.0f;
+			}
+
+			// re-scale, so delta will always be between 0 and 1
+			min = Mathf.Min (min, delta);
+			max = Mathf.Max (max, delta);
+			delta = (delta - min) / (max-min);
+
+			// randomly move along the normal
+			var n = baseNormals [i];
+			v = u + n * scale * delta;
+			vs[i] = v;
+		}
+
+		mesh.vertices = vs;
+
+		mesh.RecalculateNormals ();
+		mesh.RecalculateTangents ();
+		mesh.RecalculateBounds ();
 	}
 
 	void Deform2() {
@@ -64,9 +130,9 @@ public class NoisyMesh : MonoBehaviour {
 			var inY = theta;
 			var inZ = Mathf.PI;
 
-			var dx = noise.Noise (inX + noiseDx, inY + noiseDx, inZ + noiseDx);
-			var dy = noise.Noise (inX + noiseDy, inY + noiseDy, inZ + noiseDy);
-			var dz = noise.Noise (inX + noiseDz, inY + noiseDz, inZ + noiseDz);
+			var dx = perlin.Noise (inX + noiseDx, inY + noiseDx, inZ + noiseDx);
+			var dy = perlin.Noise (inX + noiseDy, inY + noiseDy, inZ + noiseDy);
+			var dz = perlin.Noise (inX + noiseDz, inY + noiseDz, inZ + noiseDz);
 
 			v.x += dx * scale;
 			v.y += dy * scale;
@@ -112,9 +178,9 @@ public class NoisyMesh : MonoBehaviour {
 			var inY = v.y;
 			var inZ = v.z;
 
-			var dx = noise.Noise (inX + noiseDx, inY + noiseDx, inZ + noiseDx);
-			var dy = noise.Noise (inX + noiseDy, inY + noiseDy, inZ + noiseDy);
-			var dz = noise.Noise (inX + noiseDz, inY + noiseDz, inZ + noiseDz);
+			var dx = perlin.Noise (inX + noiseDx, inY + noiseDx, inZ + noiseDx);
+			var dy = perlin.Noise (inX + noiseDy, inY + noiseDy, inZ + noiseDy);
+			var dz = perlin.Noise (inX + noiseDz, inY + noiseDz, inZ + noiseDz);
 
 			v.x += dx * scale;
 			v.y += dy * scale;
