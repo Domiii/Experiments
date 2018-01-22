6fd7de853b0520efe2c592b42f05eb446dec1104
Domiii <dominik.seifert@gmail.com>
2018-01-23 01:04:00 +0800

    - added #8 SimpleLowPolyTerrain

---
+++ 08_SimpleLowPolyTerrain/Assets/Biome.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// A Biome provides a unique stylization of Terrain
+/// </summary>
+public class Biome : MonoBehaviour
+{
+	public Color color;
+	public float height = 1;
+
+	void Reset() {
+		Randomize ();
+	}
+
+	public void Randomize ()
+	{
+		color = new Color (Random.Range (0.0f, 1), Random.Range (0.0f, 1), Random.Range (0.0f, 1));
+
+		#if UNITY_EDITOR
+		UnityEditor.SceneManagement.EditorSceneManager.MarkAllScenesDirty();
+		#endif
+	}
+}
---
+++ 08_SimpleLowPolyTerrain/Assets/Editor/SubdivisionEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Subdivision))]
+public class SubdivisionEditor : Editor {
+	public override void OnInspectorGUI ()
+	{
+		var b = (Subdivision)target;
+
+		if (GUILayout.Button ("Subdivide!")) {
+			b.Build ();
+		}
+
+		base.OnInspectorGUI ();
+	}
+}
